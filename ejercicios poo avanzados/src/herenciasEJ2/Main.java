c2f9d1adc60581359b6853279b3ff498a1701c5a
fernando-cg <72653561+fernando-cg@users.noreply.github.com>
2021-03-02 09:22:17 +0100

    herencias

--- ejercicios poo avanzados/src/herenciasEJ2/Main.java
+++ ejercicios poo avanzados/src/herenciasEJ2/Main.java
@@ -3,8 +3,15 @@ package herenciasEJ2;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Comercial com = new Comercial("pepe",50,5) ;
+		
+		Repartidor rep = new Repartidor("pepe",50,3,6) ;
 
+		
+		System.out.println(com.toString()) ;
+		
+		System.out.println(rep.toString());
 	}
 
 }
--- ejercicios poo avanzados/src/herenciasEJ2/Repartidor.java
+++ ejercicios poo avanzados/src/herenciasEJ2/Repartidor.java
@@ -1,5 +1,52 @@
 package herenciasEJ2;
 
-public class Repartidor {
+public class Repartidor extends Empleado {
+
+	private int zona ;
+	
+	private int entregas ;
+	
+	public Repartidor(String nombre, int edad, int zona , int entregas) {
+		
+		super(nombre, edad);
+		
+		setSalario(entregas*12) ;
+		
+		this.zona = zona ;
+		
+		this.entregas = entregas ;
+		
+		if((zona == 2 || zona== 3) && edad>30 && entregas >5) {
+			
+			setPlus(300) ;
+		}
+		
+	}
+
+	public int getZona() {
+		return zona;
+	}
+
+	public void setZona(int zona) {
+		this.zona = zona;
+	}
+
+	public int getEntregas() {
+		return entregas;
+	}
+
+	public void setEntregas(int entregas) {
+		this.entregas = entregas;
+	}
+
+	@Override
+	public String toString() {
+		return "Repartidor [zona=" + zona + ", entregas=" + entregas + ", getNombre()=" + getNombre() + ", getEdad()="
+				+ getEdad() + ", getSalario()=" + getSalario() + ", getPlus()=" + getPlus() + "]";
+	}
+	
+	
+	
+	
 
 }
